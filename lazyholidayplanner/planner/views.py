c4f7e82d23be3be0b6dbebb25ae154f340b284ef
Joe <josephbroseph7@gmail.com>
2018-10-20 15:12:00 +0100

    Add initial form response parsing

--- lazyholidayplanner/planner/views.py
+++ lazyholidayplanner/planner/views.py
@@ -2,13 +2,35 @@ import copy, json, datetime
 
 from django.utils import timezone
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+
+from skyscanner.skyscanner import Flights
 
 # Create your views here.
 
 def index(request):
+    # read keys in from file, and strip newline characters
+    # 0 is flight api key
+    # 1 is flight app id
+    # 2 is hotel api key
+    with open('keys') as f:
+        keys = f.readlines()
+    
+    keys = [key.strip() for key in keys]
+
+    flights_service = Flights(keys[0])
+    # places = flights_service.make_request('http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/UK/GBP/en-GB?query=manc&apiKey={keys[0]}')
+    # return HttpResponse(places)
+    
+    # result = flights_service.get_result(
+    #     country='UK',
+    #     currency='GBP',
+    #     local='en-GB',
+    #     originplace=''
+    # )
+
     return render_to_response('index.html')
 
 def new(request):
@@ -22,7 +44,15 @@ Webhook for handling information submitted to typeform. The url is '.../api/type
 def typeform_result(request):
     jsondata = request.body
     data = json.loads(jsondata)
+    query_answers = data['form_response']['answers']
     # meta = copy.copy(request.META)
+
+    trip = {}
+    trip['location'] = query_answers[0]['text']
+    trip['start_date'] = query_answers[1]['date']
+    trip['end_date'] = query_answers[2]['date']
+    trip['party_size'] = query_answers[3]['number']
+    trip['budget'] = query_answers[4]['choice']['label']
     print(data)
 
     """
